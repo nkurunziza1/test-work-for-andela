b3e7f94108d0be9247069753df46cf2f0532003d
nkurunziza1 <Alexendre.Nkurunziza@gmail.com>
2023-03-08 15:24:34 -1000

    first andela test work

---
+++ my-file.js
@@ -0,0 +1,13 @@
+/**
+ * This is a simple function that adds two numbers together and returns the result.
+ * @param {number} num1 - The first number to add.
+ * @param {number} num2 - The second number to add.
+ * @returns {number} - The sum of num1 and num2.
+ */
+function addNumbers(num1, num2) {
+  const sum = num1 + num2;
+  return sum;
+}
+
+// Test the function
+console.log(addNumbers(5, 10)); // Should output 15
